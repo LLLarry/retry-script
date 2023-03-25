b4079ca77c88bb4d2f95e8332cd01ce74c88aefa
LLLarry <1079485906@qq,com>
2023-03-25 10:50:41 +0800

    '更改配置项'

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ export default {
     //    // compact: true
     // },
     {
-      file: 'dist/retry-script.iife.js',
+      file: 'dist/retry-script.js',
       format: 'iife',
       name: 'RetryScript',
       // compact: true
@@ -26,7 +26,7 @@ export default {
   plugins: [
       resolve(),  // 这样 Rollup 能找到 `ms`
       commonjs(), // 这样 Rollup 能转换 `ms` 为一个ES模块
-      // terser(), // 生产环境下压缩代码
+      terser(), // 生产环境下压缩代码
       babel({babelHelpers: 'bundled'}),
   ]
 };
